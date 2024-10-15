57ea11777fc0783ea3da9d2cf9c1306510c51f6d
Tarun Khurana <tarunkhurana1982@gmail.com>
2024-10-15 15:59:39 -0500

    initial project commit

---
+++ ImageViewer/ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Tarun Khurana on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
